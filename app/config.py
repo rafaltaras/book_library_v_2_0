d1e712e94c9d247638981f10ed6f3a6fe37fbc01
Taraska Rafał - Korpo <Rafal.Taraska@orange.com>
2023-01-14 12:46:50 +0100

    v1.8

--- app/config.py
+++ app/config.py
@@ -1,4 +1,3 @@
-# config.py
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))    # 1
@@ -7,6 +6,6 @@ class Config:
    SECRET_KEY = os.environ.get("SECRET_KEY") or "asdvdasdasdc"    # 2
    SQLALCHEMY_DATABASE_URI = (                           # 3
            os.environ.get('DATABASE_URL') or
-           'sqlite:///' + os.path.join(BASE_DIR, 'mikroblog.db')
+           'sqlite:///' + os.path.join(BASE_DIR, 'book_library.db')
    )
    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- migrations/versions/394ed5c869b6_.py
+++ migrations/versions/394ed5c869b6_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 9e4b5bb3b0de
+Revision ID: 394ed5c869b6
 Revises: 
-Create Date: 2023-01-11 17:15:01.665093
+Create Date: 2023-01-14 12:43:11.958578
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9e4b5bb3b0de'
+revision = '394ed5c869b6'
 down_revision = None
 branch_labels = None
 depends_on = None
